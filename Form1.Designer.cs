9dac9cb7ed3a2d5876d6e8a89737cd50270c3ba4
opilane <opilane@HP-CZC6297B9K>
2020-12-11 15:37:24 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Tooded_DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.Toodetxt = new System.Windows.Forms.TextBox();
+            this.Kogustxt = new System.Windows.Forms.TextBox();
+            this.Hindtxt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_LisaPilt = new System.Windows.Forms.Button();
+            this.pilt_nim = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.btn_Insert.Location = new System.Drawing.Point(12, 326);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(105, 75);
+            this.btn_Insert.TabIndex = 0;
+            this.btn_Insert.Text = "Lisa uus";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.btn_Update.Location = new System.Drawing.Point(123, 326);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(105, 75);
+            this.btn_Update.TabIndex = 1;
+            this.btn_Update.Text = "Uuenda";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.btn_Delete.Location = new System.Drawing.Point(238, 326);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(105, 75);
+            this.btn_Delete.TabIndex = 2;
+            this.btn_Delete.Text = "Kustuta";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // Toodetxt
+            // 
+            this.Toodetxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Toodetxt.Location = new System.Drawing.Point(83, 13);
+            this.Toodetxt.Name = "Toodetxt";
+            this.Toodetxt.Size = new System.Drawing.Size(190, 32);
+            this.Toodetxt.TabIndex = 3;
+            // 
+            // Kogustxt
+            // 
+            this.Kogustxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kogustxt.Location = new System.Drawing.Point(83, 63);
+            this.Kogustxt.Name = "Kogustxt";
+            this.Kogustxt.Size = new System.Drawing.Size(190, 32);
+            this.Kogustxt.TabIndex = 4;
+            // 
+            // Hindtxt
+            // 
+            this.Hindtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Hindtxt.Location = new System.Drawing.Point(83, 105);
+            this.Hindtxt.Name = "Hindtxt";
+            this.Hindtxt.Size = new System.Drawing.Size(190, 32);
+            this.Hindtxt.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(313, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 307);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Toode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Kogus";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hind";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 198);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_LisaPilt
+            // 
+            this.btn_LisaPilt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.btn_LisaPilt.Location = new System.Drawing.Point(202, 265);
+            this.btn_LisaPilt.Name = "btn_LisaPilt";
+            this.btn_LisaPilt.Size = new System.Drawing.Size(105, 55);
+            this.btn_LisaPilt.TabIndex = 11;
+            this.btn_LisaPilt.Text = "Lisa pilt";
+            this.btn_LisaPilt.UseVisualStyleBackColor = true;
+            this.btn_LisaPilt.Click += new System.EventHandler(this.btn_LisaPilt_Click);
+            // 
+            // pilt_nim
+            // 
+            this.pilt_nim.AutoSize = true;
+            this.pilt_nim.Location = new System.Drawing.Point(238, 184);
+            this.pilt_nim.Name = "pilt_nim";
+            this.pilt_nim.Size = new System.Drawing.Size(16, 13);
+            this.pilt_nim.TabIndex = 12;
+            this.pilt_nim.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button1.Image = global::Tooded_DB.Properties.Resources.close_box_red;
+            this.button1.Location = new System.Drawing.Point(349, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 75);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(83, 144);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 32);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Kategooria";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 413);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pilt_nim);
+            this.Controls.Add(this.btn_LisaPilt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Hindtxt);
+            this.Controls.Add(this.Kogustxt);
+            this.Controls.Add(this.Toodetxt);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Name = "Form1";
+            this.Text = "Pood";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.TextBox Toodetxt;
+        private System.Windows.Forms.TextBox Kogustxt;
+        private System.Windows.Forms.TextBox Hindtxt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_LisaPilt;
+        private System.Windows.Forms.Label pilt_nim;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tooded_DB
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =|DataDirectory|\AppData\Tooded.mdf; Integrated Security = True");
+        SqlCommand command;
+        SqlDataAdapter adapter, adapter2;
+        int Id = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayData();
+        }
+        private BindingSource bindingSource1 = new BindingSource();
+        private void DisplayData()
+        {
+            connect.Open();
+            DataTable tabel = new DataTable();
+            adapter = new SqlDataAdapter("SELECT * FROM Toodetable", connect);
+            adapter.Fill(tabel);
+            dataGridView1.DataSource = tabel;
+            pictureBox1.Image = Image.FromFile("../../Images/list.png");
+           
+            adapter2 = new SqlDataAdapter("SELECT Kategooria_nimetus FROM Kategooria", connect);
+            DataTable kat_tabel = new DataTable();
+            adapter2.Fill(kat_tabel);
+            foreach (DataRow row in kat_tabel.Rows)
+            {
+                comboBox1.Items.Add(row["Kategooria_nimetus"]);
+            }           
+            connect.Close();
+        }
+        private void ClearData()
+        {
+            Toodetxt.Text = "";
+            Kogustxt.Text = "";
+            Hindtxt.Text = "";
+            pictureBox1.Image = Image.FromFile("../../Images/list.png");
+
+        }
+
+        
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            if (Toodetxt.Text != "" && Kogustxt.Text != "" && Hindtxt.Text != "" && comboBox1.SelectedItem != null)
+            {
+                try
+                {
+                    command = new SqlCommand("INSERT INTO Toodetable(Nimi,Tel,E-mail,Foto,Kategooria_Id) VALUES(@toode,@kogus,@hind,@pilt,@kat)", connect);
+                    connect.Open();
+                    command.Parameters.AddWithValue("@toode", Toodetxt.Text);
+                    command.Parameters.AddWithValue("@kogus", Kogustxt.Text);
+                    command.Parameters.AddWithValue("@hind", Hindtxt.Text);
+                    string file_pilt = Toodetxt.Text+".jpg";
+                    command.Parameters.AddWithValue("@pilt", file_pilt);
+                    command.Parameters.AddWithValue("@kat", (comboBox1.SelectedIndex+1));
+                    command.ExecuteNonQuery();
+                    connect.Close();
+                    DisplayData();
+                    ClearData();
+                    MessageBox.Show("Andmed on lisatud");
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Viga lisamisega");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Viga else");
+            }
+        }
+        SaveFileDialog save;
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            
+            if (Toodetxt.Text != "" && Kogustxt.Text != "" && Hindtxt.Text != "" && pictureBox1.Image!=null)
+            {
+                command = new SqlCommand("UPDATE Toodetable  SET Toodenimetus=@toode,Kogus=@kogus,Hind=@hind, Pilt=@pilt WHERE Id=@id", connect);
+                connect.Open();
+                command.Parameters.AddWithValue("@id", Id);
+                command.Parameters.AddWithValue("@toode", Toodetxt.Text);
+                command.Parameters.AddWithValue("@kogus", Kogustxt.Text);
+                command.Parameters.AddWithValue("@hind", Hindtxt.Text.Replace(",","."));
+                string file_pilt = Toodetxt.Text + ".jpg";
+                command.Parameters.AddWithValue("@pilt", file_pilt);
+                command.ExecuteNonQuery();
+                connect.Close();
+                DisplayData();
+                ClearData();
+                MessageBox.Show("Andmed uuendatud");
+            }
+            else
+            {
+                MessageBox.Show("Viga");
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            Toodetxt.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            Kogustxt.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            Hindtxt.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            pictureBox1.Image=Image.FromFile(@"C:\Users\opilane\source\repos\Tooded_DB\Images\"+dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString());
+            string v = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            comboBox1.SelectedIndex = Int32.Parse(v)-1;
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            if (Id!=0)
+            {
+                command = new SqlCommand("DELETE Toodetable WHERE Id=@id", connect);
+                connect.Open();
+                command.Parameters.AddWithValue("@id", Id);
+                command.ExecuteNonQuery();
+                connect.Close();
+                DisplayData();
+                ClearData();
+                MessageBox.Show("Andmed on kustutatud");
+            }
+            else
+            {
+                MessageBox.Show("Viga kustutamisega");
+            }
+        }
+        
+        private void btn_LisaPilt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+            open.InitialDirectory = Path.GetFullPath(@"C:\Users\opilane\Pictures");
+            if (open.ShowDialog()==DialogResult.OK)
+            {
+                save = new SaveFileDialog();
+                save.FileName = Toodetxt.Text+".jpg";
+                save.Filter = "Image Files(*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+                save.InitialDirectory = Path.GetFullPath(@"C:\Users\opilane\source\repos\Tooded_DB\Images");
+              
+                if (save.ShowDialog()==DialogResult.OK)
+                {
+                    File.Copy(open.FileName, save.FileName); 
+                    save.RestoreDirectory = true;
+                    pictureBox1.Image = Image.FromFile(save.FileName); 
+                } 
+
+            }
+        }
+        string Strquery;
+        string text="";
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count-1; j++)
+                {
+                    text+=(dataGridView1.Rows[i].Cells[j].Value).ToString();
+                }
+                Strquery = @"INSERT INTO Toodetable(Id,Toodenimetus,Kogus,Hind,Pilt) " +
+                    "VALUES (" + text + ")";
+                command.CommandText = Strquery;
+                command.ExecuteNonQuery();
+            }
+            connect.Close();
+        }
+    }
+}
